a44667e4a85d6f9f178bb7b9e391d8d5964302c7
luwat <oluwatobioreniyi@gmail.com>
2022-09-21 00:12:38 +0100

    add event listener, media query

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
 const startStopBtn = document.querySelector('.start-stop-btn');
-const reset = document.querySelector('.reset-btn');
+const resetBtn = document.querySelector('.reset-btn');
+const timer = document.querySelector('.timer');
 
 let seconds = 0;
 let minutes = 0;
 let hours = 0;
+
+
+let leadingSeconds = 0;
+let leadingMinutes = 0;
+let leadingHours = 0;
+
+
+let timerInterval = null;
+let timerStatus = 'stopped';
 
 function stopWatch () {
     seconds++
@@ -17,8 +27,49 @@ function stopWatch () {
         }
     }
 
-    document.querySelector('.timer').innerText = `${hours}:${minutes}:${seconds}`;
+
+    if (seconds < 10) {
+        leadingSeconds = '0' + seconds.toString();
+    } else {
+        leadingSeconds = seconds;
+    }
+
+    if (minutes < 10) {
+        leadingMinutes = '0' + minutes.toString();
+    } else {
+        leadingMinutes = minutes;
+    }
+
+    if (hours < 10) {
+        leadingHours = '0' + hours.toString();
+    } else {
+        leadingHours = hours;
+    }
+
+    timer.innerText = `${leadingHours}:${leadingMinutes}:${leadingSeconds}`;
 }
 
 
-window.setInterval(stopWatch, 1000);
+
+startStopBtn.addEventListener('click', function () {
+    if (timerStatus === 'stopped') {
+        timerInterval = window.setInterval(stopWatch, 1000);
+        startStopBtn.innerHTML = `<i class="fa-solid fa-pause pause"></i>`;
+        timerStatus = 'started';
+    } else {
+        window.clearInterval(timerInterval)
+        startStopBtn.innerHTML = `<i class="fa-solid fa-play play"></i>`;
+        timerStatus = 'stopped';
+    }
+});
+
+resetBtn.addEventListener('click', function () {
+    clearInterval(timerInterval);
+    seconds = 0;
+    minutes = 0;
+    hours = 0;
+
+    timer.innerText = '00:00:00';
+    startStopBtn.innerHTML = `<i class="fa-solid fa-play play"></i>`;
+    timerStatus = 'stopped';
+});
